b3ddf4d4a26a56ca81d404731275fffed8fa78bc
Jordan McGuire <jordandmcguire@gmail.com>
2022-01-24 08:26:39 -0700

    Unit Test Added

---
+++ GuessTheNumber/src/test/java/GuessTheNumberTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GuessTheNumberTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void greeting() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGuess() {
+    }
+}
